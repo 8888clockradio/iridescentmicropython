6b70836a5f58408764e62a971c9ee8e5e6b288a4
George Charles Rosar II <grosar@georgerosar.com>
2022-12-31 00:25:12 -0600

    Pulse

---
+++ ports/libmicropython/IRIDESCENT/libraries/Pulse_Wobbler/PulseWobblerPulseWobblerA_samples.h
@@ -0,0 +1,48 @@
+#pragma once
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <stdint.h>
+#include <string.h>
+#include <Arduino.h>
+#include <teensy_sample_data.h>
+
+const FLASHMEM uint8_t PulseWobblerPulseWobblerA_ranges[] = {37, 37, 40, 40, 43, 43, 46, 46, 49, 49, 52, 52, 55, 55, 58, 58, 61, 61, 64, 64, 67, 67, 70, 70, 73, 73, 76, 76, 79, 79, 82, 82, 127, 127, };
+
+extern const FLASHMEM sample_data PulseWobblerPulseWobblerA_samples[34];
+const FLASHMEM instrument_data PulseWobblerPulseWobblerA = { 34, const_cast<uint8_t*>(PulseWobblerPulseWobblerA_ranges), const_cast<sample_data*>(PulseWobblerPulseWobblerA_samples) };
+
+#include "sample_PulseWobbler_PulseWobblerA036L_sample.h"
+#include "sample_PulseWobbler_PulseWobblerA036R_sample.h"
+#include "sample_PulseWobbler_PulseWobblerA039L_sample.h"
+#include "sample_PulseWobbler_PulseWobblerA039R_sample.h"
+#include "sample_PulseWobbler_PulseWobblerA042L_sample.h"
+#include "sample_PulseWobbler_PulseWobblerA042R_sample.h"
+#include "sample_PulseWobbler_PulseWobblerA045L_sample.h"
+#include "sample_PulseWobbler_PulseWobblerA045R_sample.h"
+#include "sample_PulseWobbler_PulseWobblerA048L_sample.h"
+#include "sample_PulseWobbler_PulseWobblerA048R_sample.h"
+#include "sample_PulseWobbler_PulseWobblerA051L_sample.h"
+#include "sample_PulseWobbler_PulseWobblerA051R_sample.h"
+#include "sample_PulseWobbler_PulseWobblerA054L_sample.h"
+#include "sample_PulseWobbler_PulseWobblerA054R_sample.h"
+#include "sample_PulseWobbler_PulseWobblerA057L_sample.h"
+#include "sample_PulseWobbler_PulseWobblerA057R_sample.h"
+#include "sample_PulseWobbler_PulseWobblerA060L_sample.h"
+#include "sample_PulseWobbler_PulseWobblerA060R_sample.h"
+#include "sample_PulseWobbler_PulseWobblerA063L_sample.h"
+#include "sample_PulseWobbler_PulseWobblerA063R_sample.h"
+#include "sample_PulseWobbler_PulseWobblerA066L_sample.h"
+#include "sample_PulseWobbler_PulseWobblerA066R_sample.h"
+#include "sample_PulseWobbler_PulseWobblerA069L_sample.h"
+#include "sample_PulseWobbler_PulseWobblerA069R_sample.h"
+#include "sample_PulseWobbler_PulseWobblerA072L_sample.h"
+#include "sample_PulseWobbler_PulseWobblerA072R_sample.h"
+#include "sample_PulseWobbler_PulseWobblerA075L_sample.h"
+#include "sample_PulseWobbler_PulseWobblerA075R_sample.h"
+#include "sample_PulseWobbler_PulseWobblerA078L_sample.h"
+#include "sample_PulseWobbler_PulseWobblerA078R_sample.h"
+#include "sample_PulseWobbler_PulseWobblerA081L_sample.h"
+#include "sample_PulseWobbler_PulseWobblerA081R_sample.h"
+#include "sample_PulseWobbler_PulseWobblerA084L_sample.h"
+#include "sample_PulseWobbler_PulseWobblerA084R_sample.h"
