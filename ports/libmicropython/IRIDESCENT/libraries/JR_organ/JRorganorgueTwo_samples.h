9611f3a370ac8c34ab6df91d80842a65d9b2dd40
George Charles Rosar II <grosar@georgerosar.com>
2022-12-31 00:20:56 -0600

    jr_organ

---
+++ ports/libmicropython/IRIDESCENT/libraries/JR_organ/JRorganorgueTwo_samples.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <stdint.h>
+#include <string.h>
+#include <Arduino.h>
+#include <teensy_sample_data.h>
+
+const FLASHMEM uint8_t JRorganorgueTwo_ranges[] = {59, 127, };
+
+extern const FLASHMEM sample_data JRorganorgueTwo_samples[2];
+const FLASHMEM instrument_data JRorganorgueTwo = { 2, const_cast<uint8_t*>(JRorganorgueTwo_ranges), const_cast<sample_data*>(JRorganorgueTwo_samples) };
+
+#include "sample_JRorgan_orgue3_sample.h"
+#include "sample_JRorgan_orgue4_sample.h"
