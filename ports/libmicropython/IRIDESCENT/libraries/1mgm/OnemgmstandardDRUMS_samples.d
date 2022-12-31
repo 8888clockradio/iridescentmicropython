573e94fa27d0aa1584a871492aeb91cb066a4be5
George Charles Rosar II <grosar@georgerosar.com>
2022-12-31 00:12:02 -0600

    *.d

---
+++ ports/libmicropython/IRIDESCENT/libraries/1mgm/OnemgmstandardDRUMS_samples.d
@@ -0,0 +1,60 @@
+OnemgmstandardDRUMS_samples.o: OnemgmstandardDRUMS_samples.cpp \
+ OnemgmstandardDRUMS_samples.h \
+ /Users/georgerosar/Documents/GitHub/iridescentCoconutSynth/iridescentCoconut_micropythonMakefile/ports/mimxrt/IRIDESCENT/cores-master/teensy4/Arduino.h \
+ /Users/georgerosar/Documents/GitHub/iridescentCoconutSynth/iridescentCoconut_micropythonMakefile/ports/mimxrt/IRIDESCENT/cores-master/teensy4/WProgram.h \
+ /Users/georgerosar/Documents/GitHub/iridescentCoconutSynth/iridescentCoconut_micropythonMakefile/ports/mimxrt/IRIDESCENT/cores-master/teensy4/avr/pgmspace.h \
+ /Users/georgerosar/Documents/GitHub/iridescentCoconutSynth/iridescentCoconut_micropythonMakefile/ports/mimxrt/IRIDESCENT/cores-master/teensy4/avr/interrupt.h \
+ /Users/georgerosar/Documents/GitHub/iridescentCoconutSynth/iridescentCoconut_micropythonMakefile/ports/mimxrt/IRIDESCENT/cores-master/teensy4/avr_functions.h \
+ /Users/georgerosar/Documents/GitHub/iridescentCoconutSynth/iridescentCoconut_micropythonMakefile/ports/mimxrt/IRIDESCENT/cores-master/teensy4/wiring.h \
+ /Users/georgerosar/Documents/GitHub/iridescentCoconutSynth/iridescentCoconut_micropythonMakefile/ports/mimxrt/IRIDESCENT/cores-master/teensy4/binary.h \
+ /Users/georgerosar/Documents/GitHub/iridescentCoconutSynth/iridescentCoconut_micropythonMakefile/ports/mimxrt/IRIDESCENT/cores-master/teensy4/core_id.h \
+ /Users/georgerosar/Documents/GitHub/iridescentCoconutSynth/iridescentCoconut_micropythonMakefile/ports/mimxrt/IRIDESCENT/cores-master/teensy4/core_pins.h \
+ /Users/georgerosar/Documents/GitHub/iridescentCoconutSynth/iridescentCoconut_micropythonMakefile/ports/mimxrt/IRIDESCENT/cores-master/teensy4/imxrt.h \
+ /Users/georgerosar/Documents/GitHub/iridescentCoconutSynth/iridescentCoconut_micropythonMakefile/ports/mimxrt/IRIDESCENT/cores-master/teensy4/pins_arduino.h \
+ /Users/georgerosar/Documents/GitHub/iridescentCoconutSynth/iridescentCoconut_micropythonMakefile/ports/mimxrt/IRIDESCENT/cores-master/teensy4/HardwareSerial.h \
+ /Users/georgerosar/Documents/GitHub/iridescentCoconutSynth/iridescentCoconut_micropythonMakefile/ports/mimxrt/IRIDESCENT/cores-master/teensy4/Stream.h \
+ /Users/georgerosar/Documents/GitHub/iridescentCoconutSynth/iridescentCoconut_micropythonMakefile/ports/mimxrt/IRIDESCENT/cores-master/teensy4/Print.h \
+ /Users/georgerosar/Documents/GitHub/iridescentCoconutSynth/iridescentCoconut_micropythonMakefile/ports/mimxrt/IRIDESCENT/cores-master/teensy4/WString.h \
+ /Users/georgerosar/Documents/GitHub/iridescentCoconutSynth/iridescentCoconut_micropythonMakefile/ports/mimxrt/IRIDESCENT/cores-master/teensy4/avr/pgmspace.h \
+ /Users/georgerosar/Documents/GitHub/iridescentCoconutSynth/iridescentCoconut_micropythonMakefile/ports/mimxrt/IRIDESCENT/cores-master/teensy4/Printable.h \
+ /Users/georgerosar/Documents/GitHub/iridescentCoconutSynth/iridescentCoconut_micropythonMakefile/ports/mimxrt/IRIDESCENT/cores-master/teensy4/avr_emulation.h \
+ /Users/georgerosar/Documents/GitHub/iridescentCoconutSynth/iridescentCoconut_micropythonMakefile/ports/mimxrt/IRIDESCENT/cores-master/teensy4/usb_serial.h \
+ /Users/georgerosar/Documents/GitHub/iridescentCoconutSynth/iridescentCoconut_micropythonMakefile/ports/mimxrt/IRIDESCENT/cores-master/teensy4/usb_desc.h \
+ /Users/georgerosar/Documents/GitHub/iridescentCoconutSynth/iridescentCoconut_micropythonMakefile/ports/mimxrt/IRIDESCENT/cores-master/teensy4/usb_seremu.h \
+ /Users/georgerosar/Documents/GitHub/iridescentCoconutSynth/iridescentCoconut_micropythonMakefile/ports/mimxrt/IRIDESCENT/cores-master/teensy4/usb_keyboard.h \
+ /Users/georgerosar/Documents/GitHub/iridescentCoconutSynth/iridescentCoconut_micropythonMakefile/ports/mimxrt/IRIDESCENT/cores-master/teensy4/keylayouts.h \
+ /Users/georgerosar/Documents/GitHub/iridescentCoconutSynth/iridescentCoconut_micropythonMakefile/ports/mimxrt/IRIDESCENT/cores-master/teensy4/usb_mouse.h \
+ /Users/georgerosar/Documents/GitHub/iridescentCoconutSynth/iridescentCoconut_micropythonMakefile/ports/mimxrt/IRIDESCENT/cores-master/teensy4/usb_joystick.h \
+ /Users/georgerosar/Documents/GitHub/iridescentCoconutSynth/iridescentCoconut_micropythonMakefile/ports/mimxrt/IRIDESCENT/cores-master/teensy4/usb_midi.h \
+ /Users/georgerosar/Documents/GitHub/iridescentCoconutSynth/iridescentCoconut_micropythonMakefile/ports/mimxrt/IRIDESCENT/cores-master/teensy4/usb_rawhid.h \
+ /Users/georgerosar/Documents/GitHub/iridescentCoconutSynth/iridescentCoconut_micropythonMakefile/ports/mimxrt/IRIDESCENT/cores-master/teensy4/usb_flightsim.h \
+ /Users/georgerosar/Documents/GitHub/iridescentCoconutSynth/iridescentCoconut_micropythonMakefile/ports/mimxrt/IRIDESCENT/cores-master/teensy4/usb_audio.h \
+ /Users/georgerosar/Documents/GitHub/iridescentCoconutSynth/iridescentCoconut_micropythonMakefile/ports/mimxrt/IRIDESCENT/cores-master/teensy4/AudioStream.h \
+ /Users/georgerosar/Documents/GitHub/iridescentCoconutSynth/iridescentCoconut_micropythonMakefile/ports/mimxrt/IRIDESCENT/cores-master/teensy4/usb_touch.h \
+ /Users/georgerosar/Documents/GitHub/iridescentCoconutSynth/iridescentCoconut_micropythonMakefile/ports/mimxrt/IRIDESCENT/cores-master/teensy4/WCharacter.h \
+ /Users/georgerosar/Documents/GitHub/iridescentCoconutSynth/iridescentCoconut_micropythonMakefile/ports/mimxrt/IRIDESCENT/cores-master/teensy4/elapsedMillis.h \
+ /Users/georgerosar/Documents/GitHub/iridescentCoconutSynth/iridescentCoconut_micropythonMakefile/ports/mimxrt/IRIDESCENT/cores-master/teensy4/Arduino.h \
+ /Users/georgerosar/Documents/GitHub/iridescentCoconutSynth/iridescentCoconut_micropythonMakefile/ports/mimxrt/IRIDESCENT/cores-master/teensy4/IntervalTimer.h \
+ /Users/georgerosar/Documents/GitHub/iridescentCoconutSynth/iridescentCoconut_micropythonMakefile/ports/mimxrt/IRIDESCENT/cores-master/teensy4/CrashReport.h \
+ /Users/georgerosar/Documents/GitHub/iridescentCoconutSynth/iridescentCoconut_micropythonMakefile/ports/mimxrt/IRIDESCENT/cores-master/teensy4/Printable.h \
+ /Users/georgerosar/Documents/GitHub/iridescentCoconutSynth/iridescentCoconut_micropythonMakefile/ports/mimxrt/IRIDESCENT/cores-master/teensy4/WString.h \
+ /Users/georgerosar/Documents/GitHub/iridescentCoconutSynth/iridescentCoconut_micropythonMakefile/ports/mimxrt/IRIDESCENT/Audio/teensy_sample_data.h \
+ sample_Onemgm_filtersnap_sample.h sample_Onemgm_whitenoisewave_sample.h \
+ sample_Onemgm_verbclickwave_sample.h sample_Onemgm_scratch_sample.h \
+ sample_Onemgm_guitarfret_sample.h sample_Onemgm_stix_sample.h \
+ sample_Onemgm_sinetick_sample.h sample_Onemgm_coldglass12wave_sample.h \
+ sample_Onemgm_bd15_sample.h sample_Onemgm_snare24_sample.h \
+ sample_Onemgm_applause_sample.h sample_Onemgm_floortombrite_sample.h \
+ sample_Onemgm_hatopenms_sample.h sample_Onemgm_crash5_sample.h \
+ sample_Onemgm_paisteping_sample.h sample_Onemgm_chcrash_sample.h \
+ sample_Onemgm_rideping_sample.h sample_Onemgm_tamborine_sample.h \
+ sample_Onemgm_cowbell_sample.h sample_Onemgm_vibraloop_sample.h \
+ sample_Onemgm_ebongostone_sample.h sample_Onemgm_mbongotone_sample.h \
+ sample_Onemgm_quintoslap_sample.h sample_Onemgm_quintotone_sample.h \
+ sample_Onemgm_lowtumba_sample.h sample_Onemgm_timpani_sample.h \
+ sample_Onemgm_agogolotone_sample.h sample_Onemgm_cabasastrk_sample.h \
+ sample_Onemgm_maracas_sample.h sample_Onemgm_sambawhistle_sample.h \
+ sample_Onemgm_guirodown_sample.h sample_Onemgm_guiro2_sample.h \
+ sample_Onemgm_bockclave_sample.h sample_Onemgm_woodblock_sample.h \
+ sample_Onemgm_quicadown_sample.h sample_Onemgm_triangle_sample.h \
+ sample_Onemgm_belltree_sample.h
