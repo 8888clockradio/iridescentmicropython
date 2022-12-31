10086862c33d9c10c1b07f5cd8ce5fc57e67e6b7
George Charles Rosar II <grosar@georgerosar.com>
2022-12-31 02:21:01 -0600

    SDL

---
+++ ports/libmicropython/IRIDESCENT/SDL3-main/SDL/include/SDL3/SDL_revision.h
@@ -0,0 +1,8 @@
+/* #undef SDL_VENDOR_INFO */
+#define SDL_REVISION_NUMBER 0
+
+#ifdef SDL_VENDOR_INFO
+#define SDL_REVISION "SDL-v1.19.1-733-gad50a9384 (" SDL_VENDOR_INFO ")"
+#else
+#define SDL_REVISION "SDL-v1.19.1-733-gad50a9384"
+#endif
