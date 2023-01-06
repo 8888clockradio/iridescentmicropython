4006859d86c65a4f29eb55d3f066ebaf6fd603ba
George Charles Rosar II <grosar@georgerosar.com>
2023-01-05 23:09:02 -0600

    oops

--- ports/libmicropython/IRIDESCENT/sdl/SDL3/SDL_revision.h
+++ ports/libmicropython/IRIDESCENT/sdl/SDL3/SDL_revision.h
@@ -2,7 +2,7 @@
 #define SDL_REVISION_NUMBER 0
 
 #ifdef SDL_VENDOR_INFO
-#define SDL_REVISION "SDL-3.0.0-gc0a74ad9e (" SDL_VENDOR_INFO ")"
+#define SDL_REVISION "SDL-3.0.0-g8f0eaa06d (" SDL_VENDOR_INFO ")"
 #else
-#define SDL_REVISION "SDL-3.0.0-gc0a74ad9e"
+#define SDL_REVISION "SDL-3.0.0-g8f0eaa06d"
 #endif
