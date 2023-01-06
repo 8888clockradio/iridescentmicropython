7a5e6703bef639fa45f8064a4877732b6b374057
George Charles Rosar II <grosar@georgerosar.com>
2023-01-05 22:29:00 -0600

    git commit

---
+++ ports/libmicropython/IRIDESCENT/glew-master/toolchain.cmake
@@ -0,0 +1,14 @@
+set(CMAKE_SYSTEM_NAME Generic)
+set(CMAKE_SYSTEM_PROCESSOR arm)
+#include("/Users/iridescent/iridescent/iridescentCoconutSynth2/iridescentmicropython/toolchainSetup.cmake")
+
+
+set(CMAKE_C_COMPILER "${COMPILERPATH}/arm-none-eabi-gcc")
+set(CMAKE_CXX_COMPILER "${COMPILERPATH}/arm-none-eabi-g++")
+
+set(CMAKE_EXE_LINKER_FLAGS "-specs=nosys.specs" CACHE INTERNAL "")
+
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
