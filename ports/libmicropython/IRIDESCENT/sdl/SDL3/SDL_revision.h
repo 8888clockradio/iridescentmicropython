4ffc38dd7ae7ce9c53c349f47271254ba6132714
George Charles Rosar II <grosar@georgerosar.com>
2023-01-06 02:52:19 -0600

    reinit_from_micro_python_original

--- ports/libmicropython/IRIDESCENT/sdl/SDL3/SDL_revision.h
+++ ports/libmicropython/IRIDESCENT/sdl/SDL3/SDL_revision.h
@@ -2,7 +2,7 @@
 #define SDL_REVISION_NUMBER 0
 
 #ifdef SDL_VENDOR_INFO
-#define SDL_REVISION "SDL-3.0.0-g0550763a5 (" SDL_VENDOR_INFO ")"
+#define SDL_REVISION "SDL-3.0.0-gc5fae2fd2 (" SDL_VENDOR_INFO ")"
 #else
-#define SDL_REVISION "SDL-3.0.0-g0550763a5"
+#define SDL_REVISION "SDL-3.0.0-gc5fae2fd2"
 #endif
