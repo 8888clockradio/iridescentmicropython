c0a74ad9ea5de7765e9d50937ac0398d9c98e814
George Charles Rosar II <grosar@georgerosar.com>
2023-01-05 23:02:43 -0600

    fix

--- SDL/include/SDL3/SDL_revision.h
+++ SDL/include/SDL3/SDL_revision.h
@@ -2,7 +2,7 @@
 #define SDL_REVISION_NUMBER 0
 
 #ifdef SDL_VENDOR_INFO
-#define SDL_REVISION "SDL-3.0.0-g10cbc6561 (" SDL_VENDOR_INFO ")"
+#define SDL_REVISION "SDL-3.0.0-g4cf418491 (" SDL_VENDOR_INFO ")"
 #else
-#define SDL_REVISION "SDL-3.0.0-g10cbc6561"
+#define SDL_REVISION "SDL-3.0.0-g4cf418491"
 #endif
