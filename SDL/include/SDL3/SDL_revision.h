8959f3503b2c62edb0c91ac6cf199466b814d35d
George Charles Rosar II <grosar@georgerosar.com>
2023-01-07 15:33:07 -0600

    toolchain update

--- SDL/include/SDL3/SDL_revision.h
+++ SDL/include/SDL3/SDL_revision.h
@@ -2,7 +2,7 @@
 #define SDL_REVISION_NUMBER 0
 
 #ifdef SDL_VENDOR_INFO
-#define SDL_REVISION "SDL-3.0.0-gcfd268727 (" SDL_VENDOR_INFO ")"
+#define SDL_REVISION "SDL-3.0.0-g59bbfdcc2 (" SDL_VENDOR_INFO ")"
 #else
-#define SDL_REVISION "SDL-3.0.0-gcfd268727"
+#define SDL_REVISION "SDL-3.0.0-g59bbfdcc2"
 #endif
