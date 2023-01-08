1052f7da37c1817b10a9c3857450d1cc2c88b0c6
George Charles Rosar II <grosar@georgerosar.com>
2023-01-07 22:10:01 -0600

    arm-unknown-linux-gnueabi

--- ports/libmicropython/IRIDESCENT/qhull-2020.2/src/qconvex/qconvex.d
+++ ports/libmicropython/IRIDESCENT/qhull-2020.2/src/qconvex/qconvex.d
@@ -1,3 +1,38 @@
 src/qconvex/qconvex.o: src/qconvex/qconvex.c src/libqhull/libqhull.h \
- src/libqhull/user.h src/libqhull/mem.h src/libqhull/qset.h \
- src/libqhull/stat.h
+ src/libqhull/user.h \
+ /Users/iridescent/iridescent/iridescentCoconutSynth2/iridescentmicropython/ports/libmicropython/IRIDESCENT/qhull-2020.2/../newlib_code/arm-unknown-linux-gnueabi/include/limits.h \
+ /Users/iridescent/iridescent/iridescentCoconutSynth2/iridescentmicropython/ports/libmicropython/IRIDESCENT/qhull-2020.2/../newlib_code/arm-unknown-linux-gnueabi/include/newlib.h \
+ /Users/iridescent/iridescent/iridescentCoconutSynth2/iridescentmicropython/ports/libmicropython/IRIDESCENT/qhull-2020.2/../newlib_code/arm-unknown-linux-gnueabi/include/_newlib_version.h \
+ /Users/iridescent/iridescent/iridescentCoconutSynth2/iridescentmicropython/ports/libmicropython/IRIDESCENT/qhull-2020.2/../newlib_code/arm-unknown-linux-gnueabi/include/sys/cdefs.h \
+ /Users/iridescent/iridescent/iridescentCoconutSynth2/iridescentmicropython/ports/libmicropython/IRIDESCENT/qhull-2020.2/../newlib_code/arm-unknown-linux-gnueabi/include/machine/_default_types.h \
+ /Users/iridescent/iridescent/iridescentCoconutSynth2/iridescentmicropython/ports/libmicropython/IRIDESCENT/qhull-2020.2/../newlib_code/arm-unknown-linux-gnueabi/include/sys/features.h \
+ /Users/iridescent/iridescent/iridescentCoconutSynth2/iridescentmicropython/ports/libmicropython/IRIDESCENT/qhull-2020.2/../newlib_code/arm-unknown-linux-gnueabi/include/sys/syslimits.h \
+ /Users/iridescent/iridescent/iridescentCoconutSynth2/iridescentmicropython/ports/libmicropython/IRIDESCENT/qhull-2020.2/../newlib_code/arm-unknown-linux-gnueabi/include/time.h \
+ /Users/iridescent/iridescent/iridescentCoconutSynth2/iridescentmicropython/ports/libmicropython/IRIDESCENT/qhull-2020.2/../newlib_code/arm-unknown-linux-gnueabi/include/_ansi.h \
+ /Users/iridescent/iridescent/iridescentCoconutSynth2/iridescentmicropython/ports/libmicropython/IRIDESCENT/qhull-2020.2/../newlib_code/arm-unknown-linux-gnueabi/include/sys/config.h \
+ /Users/iridescent/iridescent/iridescentCoconutSynth2/iridescentmicropython/ports/libmicropython/IRIDESCENT/qhull-2020.2/../newlib_code/arm-unknown-linux-gnueabi/include/machine/ieeefp.h \
+ /Users/iridescent/iridescent/iridescentCoconutSynth2/iridescentmicropython/ports/libmicropython/IRIDESCENT/qhull-2020.2/../newlib_code/arm-unknown-linux-gnueabi/include/sys/reent.h \
+ /Users/iridescent/iridescent/iridescentCoconutSynth2/iridescentmicropython/ports/libmicropython/IRIDESCENT/qhull-2020.2/../newlib_code/arm-unknown-linux-gnueabi/include/_ansi.h \
+ /Users/iridescent/iridescent/iridescentCoconutSynth2/iridescentmicropython/ports/libmicropython/IRIDESCENT/qhull-2020.2/../newlib_code/arm-unknown-linux-gnueabi/include/sys/_types.h \
+ /Users/iridescent/iridescent/iridescentCoconutSynth2/iridescentmicropython/ports/libmicropython/IRIDESCENT/qhull-2020.2/../newlib_code/arm-unknown-linux-gnueabi/include/machine/_types.h \
+ /Users/iridescent/iridescent/iridescentCoconutSynth2/iridescentmicropython/ports/libmicropython/IRIDESCENT/qhull-2020.2/../newlib_code/arm-unknown-linux-gnueabi/include/sys/lock.h \
+ /Users/iridescent/iridescent/iridescentCoconutSynth2/iridescentmicropython/ports/libmicropython/IRIDESCENT/qhull-2020.2/../newlib_code/arm-unknown-linux-gnueabi/include/machine/time.h \
+ /Users/iridescent/iridescent/iridescentCoconutSynth2/iridescentmicropython/ports/libmicropython/IRIDESCENT/qhull-2020.2/../newlib_code/arm-unknown-linux-gnueabi/include/sys/types.h \
+ /Users/iridescent/iridescent/iridescentCoconutSynth2/iridescentmicropython/ports/libmicropython/IRIDESCENT/qhull-2020.2/../newlib_code/arm-unknown-linux-gnueabi/include/sys/_stdint.h \
+ /Users/iridescent/iridescent/iridescentCoconutSynth2/iridescentmicropython/ports/libmicropython/IRIDESCENT/qhull-2020.2/../newlib_code/arm-unknown-linux-gnueabi/include/sys/_pthreadtypes.h \
+ /Users/iridescent/iridescent/iridescentCoconutSynth2/iridescentmicropython/ports/libmicropython/IRIDESCENT/qhull-2020.2/../newlib_code/arm-unknown-linux-gnueabi/include/machine/types.h \
+ /Users/iridescent/iridescent/iridescentCoconutSynth2/iridescentmicropython/ports/libmicropython/IRIDESCENT/qhull-2020.2/../newlib_code/arm-unknown-linux-gnueabi/include/sys/timespec.h \
+ /Users/iridescent/iridescent/iridescentCoconutSynth2/iridescentmicropython/ports/libmicropython/IRIDESCENT/qhull-2020.2/../newlib_code/arm-unknown-linux-gnueabi/include/sys/_timespec.h \
+ src/libqhull/mem.h \
+ /Users/iridescent/iridescent/iridescentCoconutSynth2/iridescentmicropython/ports/libmicropython/IRIDESCENT/qhull-2020.2/../newlib_code/arm-unknown-linux-gnueabi/include/stdio.h \
+ /Users/iridescent/iridescent/iridescentCoconutSynth2/iridescentmicropython/ports/libmicropython/IRIDESCENT/qhull-2020.2/../newlib_code/arm-unknown-linux-gnueabi/include/sys/stdio.h \
+ src/libqhull/qset.h \
+ /Users/iridescent/iridescent/iridescentCoconutSynth2/iridescentmicropython/ports/libmicropython/IRIDESCENT/qhull-2020.2/../newlib_code/arm-unknown-linux-gnueabi/include/setjmp.h \
+ /Users/iridescent/iridescent/iridescentCoconutSynth2/iridescentmicropython/ports/libmicropython/IRIDESCENT/qhull-2020.2/../newlib_code/arm-unknown-linux-gnueabi/include/machine/setjmp.h \
+ src/libqhull/stat.h \
+ /Users/iridescent/iridescent/iridescentCoconutSynth2/iridescentmicropython/ports/libmicropython/IRIDESCENT/qhull-2020.2/../newlib_code/arm-unknown-linux-gnueabi/include/stdlib.h \
+ /Users/iridescent/iridescent/iridescentCoconutSynth2/iridescentmicropython/ports/libmicropython/IRIDESCENT/qhull-2020.2/../newlib_code/arm-unknown-linux-gnueabi/include/machine/stdlib.h \
+ /Users/iridescent/iridescent/iridescentCoconutSynth2/iridescentmicropython/ports/libmicropython/IRIDESCENT/qhull-2020.2/../newlib_code/arm-unknown-linux-gnueabi/include/string.h \
+ /Users/iridescent/iridescent/iridescentCoconutSynth2/iridescentmicropython/ports/libmicropython/IRIDESCENT/qhull-2020.2/../newlib_code/arm-unknown-linux-gnueabi/include/sys/string.h \
+ /Users/iridescent/iridescent/iridescentCoconutSynth2/iridescentmicropython/ports/libmicropython/IRIDESCENT/qhull-2020.2/../newlib_code/arm-unknown-linux-gnueabi/include/ctype.h \
+ /Users/iridescent/iridescent/iridescentCoconutSynth2/iridescentmicropython/ports/libmicropython/IRIDESCENT/qhull-2020.2/../newlib_code/arm-unknown-linux-gnueabi/include/math.h
